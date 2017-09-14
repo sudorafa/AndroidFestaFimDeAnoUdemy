51aea503c685a1f039c4f72e9148f741c5069772
o Rafa <paracafa@gmail.com>
2017-09-13 21:25:38 -0300

    Aula 25 - Navegando Entre Activities *Intent*

--- app/src/main/java/com/example/orafa/androidfestafimdeanoudemy/MainActivity.java
+++ app/src/main/java/com/example/orafa/androidfestafimdeanoudemy/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.orafa.androidfestafimdeanoudemy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.mViewHolder.textToday = (TextView) findViewById(R.id.text_today);
+        this.mViewHolder.textToday = (TextView) findViewById(R.id.text_days_left);
+        this.mViewHolder.textToday = (Button) findViewById(R.id.button_confirm);
+
+        this.mViewHolder.buttonConfirm.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.button_confirm){
+            Intent intent = new Intent(this, DetailsActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    private static class ViewHolder{
+        TextView textToday;
+        TextView textDaysLeft;
+        Button buttonConfirm;
     }
 }
